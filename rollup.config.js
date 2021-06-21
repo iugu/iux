9388a967e029b1945f5999f76312ee1e63910955
Thiago Vian <madebyvian@gmail.com>
2021-06-21 12:10:42 -0300

    Adicionando Babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
-export default {
+const { babel } = require('@rollup/plugin-babel');
+module.exports = {
   input: 'src/js/main.js',
   output: {
     file: 'build/js/iux.min.js',
     format: 'iife'
-  }
+  },
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+    }),
+  ],
 };
